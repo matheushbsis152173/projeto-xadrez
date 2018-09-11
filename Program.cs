fdd474d2f77ce961448adddb865854dc6819b83b
matheus santos <senaigincana8@gmail.com>
2018-09-11 08:52:52 -0300

    acrescentei nova mensagem

--- Program.cs
+++ Program.cs
@@ -7,20 +7,8 @@ using System.Threading.Tasks;
 namespace Projeto_Xadrez {
     class Program {
         static void Main(string[] args) {
-            int V_inicial, vazao, tempo, V_Final;
-
-            V_inicial = int.Parse(Console.ReadLine());
-            vazao = int.Parse(Console.ReadLine());
-            tempo = int.Parse(Console.ReadLine());
-
-            if (vazao * tempo > V_inicial) {
-                Console.WriteLine("Não é possivel");
-            }
-            else {
-                V_Final = V_inicial - (vazao * tempo);
-                Console.WriteLine(V_Final);
-            }
-            Console.ReadLine();
+            Console.WriteLine("hello");
+            Console.WriteLine("good nigth!");
         }
     }
 }
