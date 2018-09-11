4db8e915ff31a6a69ccbb36a16f3d8ea14bac8e5
matheus santos <senaigincana8@gmail.com>
2018-09-11 08:30:53 -0300

    projeto inicado, mostrando a mensagem inicial

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Xadrez {
+    class Program {
+        static void Main(string[] args) {
+            int V_inicial, vazao, tempo, V_Final;
+
+            V_inicial = int.Parse(Console.ReadLine());
+            vazao = int.Parse(Console.ReadLine());
+            tempo = int.Parse(Console.ReadLine());
+
+            if (vazao * tempo > V_inicial) {
+                Console.WriteLine("Não é possivel");
+            }
+            else {
+                V_Final = V_inicial - (vazao * tempo);
+                Console.WriteLine(V_Final);
+            }
+            Console.ReadLine();
+        }
+    }
+}
